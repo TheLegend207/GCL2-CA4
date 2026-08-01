918cb18699a1dcac220a0c999cc753067c79d6bf
JoshyMoshy07 <joshuasong1925@gmail.com>
2026-08-01 11:44:31 +0800

    Main Menu and Credits complete
    might improve on it later to make it look like the actual game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuCam : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     void Update()
     {
         transform.Rotate(0f, 10f * Time.deltaTime, 0f); //Rotate camera constantly
+    }
+
+    //Load to different scenes when pressing their respective buttons
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Level 01");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
