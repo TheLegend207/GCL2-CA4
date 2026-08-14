155938ca8263c3157d28b2ec6b11afd19e1a32c3
Sanjeev Shanmugasundaram <sanje6080j@gmail.com>
2026-08-14 13:00:23 +0800

    Added exit and win scene

---
+++ Assets/Exitdoor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitDoor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Win screen");
+        }
+    }
+}
